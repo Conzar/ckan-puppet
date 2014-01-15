5884c10c3a0773eeac66c4f2e299f989dcafd565
Michael Speth <spethm@landcareresearch.co.nz>
2014-01-15 15:00:27 +1300

    New puppet forge release.

---
+++ pkg/conzar-ckan-1.0.1/manifests/repos.pp
@@ -0,0 +1,23 @@
+# repos for ckan install
+class ckan::repos {
+
+  package {'python-software-properties':
+    ensure => installed,
+  }
+
+  file {$ckan::ckan_package_dir:
+    ensure => directory,
+  }
+
+  # downloads the package from the specified source.
+  if $ckan::is_ckan_from_repo == 'false' {
+    include wget
+    wget::fetch { 'Download ckan package':
+      source      => $ckan::ckan_package_url,
+      destination => "$ckan::ckan_package_dir/$ckan::ckan_package_filename",
+      timeout     => 0,
+      verbose     => false,
+      require     => File[$ckan::ckan_package_dir],
+    }
+  }
+}
