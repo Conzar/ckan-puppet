eb1aa8558ce0466b4be55761104c444471db39d2
Nick Stenning <nick@whiteink.com>
2014-07-24 14:27:46 +0200

    Add some better documentation to the provided CKAN extensions

--- manifests/ext/spatial.pp
+++ manifests/ext/spatial.pp
@@ -1,4 +1,15 @@
-# installs the ckanext-spatial extension
+# == Class: ckan::ext::spatial
+#
+# Installs the "spatial" extension, which allows for the association of datasets
+# with geographic locations, and for searches across datasets to be restricted
+# to a particular geographical area. Additionally, it provides some support for
+# previewing geographical datasets.
+#
+# At a minimum, you should enable the "spatial_metadata" and "spatial_query"
+# plugins. See the plugin documentation for full details:
+#
+#   http://docs.ckan.org/projects/ckanext-spatial/en/latest/
+#
 class ckan::ext::spatial {
 
   $ckanext_spatial_libs = [
