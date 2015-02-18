dffb6fb1d5f6a100ddfba6e6a493be63dac92b96
Michael Speth <spethm@landcareresearch.co.nz>
2015-02-19 11:42:51 +1300

    fixed issue with fact not reporting correctly if plugins.out file doesn't exist

--- lib/facter/ckan_production_plugins.rb
+++ lib/facter/ckan_production_plugins.rb
@@ -1,3 +1,7 @@
 Facter.add('ckan_production_plugins') do
-  setcode 'cat /etc/ckan/plugins.out'
+  setcode do
+  	if File.exist? '/etc/ckan/plugins.out'
+  		Facter::Core::Execution.exec('cat /etc/ckan/plugins.out')
+  	end
+  end
 end
